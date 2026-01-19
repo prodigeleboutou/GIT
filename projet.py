77242424a7ad9c0f579ebf4e7bd2f3076b87f70a
prodigeleboutou <prodigeleboutou@gmail.com>
2026-01-20 00:04:53 +0100

    second commit

---
+++ projet.py
@@ -0,0 +1,22 @@
+# commande git log permet de voir tous les commits éffectués
+# git branch -a pour voir toutes les branches(principal et autres)
+
+class Personne:
+    def __init__(self ,a,b,c):
+        self.a=input("entrez un nom")
+        self.b=input("entrer la nature")
+        self.c=int(input("entrer le point de vie"))
+        
+            
+    def info(self):
+        print(self.a,"est une ",self.b,"ayant",self.c,"de vie")
+
+
+
+
+
+
+
+
+perso1=Personne("Alice","Guerrière", 100)
+perso1.info()
